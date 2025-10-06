484e7ee46819248695d603ff2d84c34c80c2a68b
Georg Maißer <georg.maisser@wunderbyte.at>
2025-10-06 15:16:14 +0200

    Version (2025100600)

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
  */
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2025092500;
+$plugin->version   = 2025100600;
 $plugin->requires  = 2024042200; // Requires this Moodle version. Current: Moodle 4.4.
 $plugin->component = 'bookingextension_confirmation_supervisor';
 $plugin->supported = [404, 405];
